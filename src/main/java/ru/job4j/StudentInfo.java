18dc55f571945f9e6e11986d7a9fdc2b7bc31477
progsilenth <progsilenth@gmail.com>
2023-01-04 22:06:25 +0500

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Slava Ibragimov");
+        System.out.println("31.01.1986");
+    }
+}
